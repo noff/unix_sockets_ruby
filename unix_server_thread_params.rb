0b9c573a5f5ed7c5cabf2eb0786e8c7836151977
Michael Kechinov <sam@mkechinov.ru>
2015-04-23 08:37:48 +0300

    Server with params now threaded

--- unix_server_thread_params.rb
+++ unix_server_thread_params.rb
@@ -1,13 +1,14 @@
 require 'socket'
 
 socket_file_name = './demo.socket'
-
-loop do
   File.unlink socket_file_name if File.exist? socket_file_name
   server = UNIXServer.new socket_file_name
-  client = server.accept
-  message = client.recvfrom(65536)[0].upcase
-  client.puts "Hi, #{message}"
-  client.puts 'Closing the connection. Bye!'
-  client.close
+
+loop do
+  Thread.start(server.accept) do |client|
+	  message = client.recvfrom(120)[0].upcase
+	  client.puts "Hi, #{message}"
+	  client.puts 'Closing the connection. Bye!'
+	  client.close
+	end
 end
